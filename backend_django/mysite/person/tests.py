0e5bed1874e8de0f0ef3342802825b5b2dc5bb8d
ZiganshinIB <elzig3012@gmail.com>
2024-10-18 15:18:26 +0300

    Add test for person

--- backend_django/mysite/person/tests.py
+++ backend_django/mysite/person/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Permission, Group
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -34,18 +35,32 @@ class PersonModelTest(TestCase):
 
 class UserAPITest(APITestCase):
     def setUp(self):
+        Group.objects.create(name='hr')
+        self.hr_group = Group.objects.get(name='hr')
+        self.hr_group.permissions.add(Permission.objects.get(codename='add_person'))
+        self.hr_group.permissions.add(Permission.objects.get(codename='view_person'))
+        self.hr_group.permissions.add(Permission.objects.get(codename='change_person'))
         self.user = Person.objects.create_user(
             username='testuser',
             first_name='Test',
             last_name='User',
             password='testpassword'
         )
+        self.hr_user = Person.objects.create_user(
+            username='hruser',
+            first_name='HR',
+            last_name='User',
+            password='testpassword',
+            manager=self.user,
+            is_staff=True
+        )
+        self.hr_user.groups.add(self.hr_group)
 
 
     def test_login(self):
         url = reverse('login')
         data = {
-            'username': self.user,
+            'username': self.user.username,
             'password': 'testpassword',
         }
         response = self.client.post(url, data, format='json')
@@ -60,17 +75,98 @@ class UserAPITest(APITestCase):
         self.assertEqual(response.data['non_field_errors'], ["Невозможно войти с предоставленными учетными данными."])
 
 
-    def client_login(self):
+    def client_login(self, username='testuser', password='testpassword'):
         url = reverse('login')
         data = {
-            'username': self.user.username,
-            'password': 'testpassword',
+            'username': username,
+            'password': password,
         }
         response = self.client.post(url, data, format='json')
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {response.data["auth_token"]}')
 
     def test_logout(self):
         url = reverse('logout')
-        self.client_login()
-        response = self.client.post(url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.post(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_create_user(self):
+        url = reverse('person-list')
+        response = self.client.post(url, {'username': 'testuser1', 'first_name': 'Test', 'last_name': 'User', 'password': 'testpassword'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_user_with_hr_user(self):
+        url = reverse('person-list')
+        self.client_login(self.hr_user.username, 'testpassword')
+        data = {
+            'username': 'testuser1',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password': 'testpassword',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_users(self):
+        url = reverse('person-list')
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_users_with_hr(self):
+        url = reverse('person-list')
+        self.client_login(self.hr_user.username, 'testpassword')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_user(self):
+        url = reverse('person-detail', args=[self.hr_user.pk])
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        url = reverse('person-detail', args=[self.user.pk])
+        self.client_login(self.hr_user.username, 'testpassword')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_me(self):
+        url = reverse('person-me')
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update(self):
+        url = reverse('person-detail', args=[self.user.pk])
+        self.client_login(self.user.username, 'testpassword')
+        data = {
+            'phone_number': '+79001234567',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'surname': 'User',
+            'birthday': '2000-01-01'
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client_login(self.hr_user.username, 'testpassword')
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        url = reverse('person-detail', args=[self.user.pk])
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.client_login(self.hr_user.username, 'testpassword')
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_me(self):
+        url = reverse('person-me')
+        self.client_login(self.user.username, 'testpassword')
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+
